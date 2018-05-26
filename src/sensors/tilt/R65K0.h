da755dc1fd1b1d5c86c1b57760ce431df34bf40b
Dmytro Shynkevych <dm.shynk@gmail.com>
2018-05-26 01:56:10 -0400

    Made tilt sensor a subclass of AnalogPinSensor

--- src/sensors/tilt/R65K0.h
+++ src/sensors/tilt/R65K0.h
@@ -1,27 +1,16 @@
 #ifndef GOOSE_SENSORS_R65K0_H
 #define GOOSE_SENSORS_R65K0_H
 
-#define SCALE 0.0048828
-#define RSHUNT 250.0 // in Ohms
-
-#include <Cosa/AnalogPin.hh>
+#include "../AnalogPinSensor.h"
 
 namespace wlp {
-
-    class R65K0 {
+    class R65K0: public AnalogPinSensor {
     public:
-        explicit R65K0(Board::AnalogPin analog_pin);
-
-        double read_tilt();
-
-        void begin();
-
-        void sleep();
-
-    private:
-        AnalogPin m_analog_pin;
+        explicit R65K0(Board::AnalogPin analog_pin)
+        : AnalogPinSensor(analog_pin) {};
+    protected:
+        virtual double read_value() override;
     };
-
 }
 
 #endif //GOOSE_SENSORS_R65K0_H
